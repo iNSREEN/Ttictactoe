8f8ec0c4e8d469e9d8c97042cefd637e53013ce0
Nsreen Alfaifi <nmalfaifi01@gmail.com>
2023-02-02 14:52:55 +0300

    Initial Commit

---
+++ Ttictactoe/TtictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TtictactoeApp.swift
+//  Ttictactoe
+//
+//  Created by Nsreen Alfaifi on 11/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TtictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
